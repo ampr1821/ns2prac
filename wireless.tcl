35fd9889bfb791fa3243ab58bc2ff23b33db76b9
Amitash M S <44923742+Am271@users.noreply.github.com>
2021-04-29 12:21:42 +0530

    Update wireless.tcl

--- wireless.tcl
+++ wireless.tcl
@@ -1,163 +1,138 @@
-#======================================================================
-# Define options
-#======================================================================
- set val(chan)         Channel/WirelessChannel  ;# channel type
- set val(prop)         Propagation/TwoRayGround ;# radio-propagation model
- set val(ant)          Antenna/OmniAntenna      ;# Antenna type
- set val(ll)           LL                       ;# Link layer type
- set val(ifq)          Queue/DropTail/PriQueue  ;# Interface queue type
- set val(ifqlen)       50                       ;# max packet in ifq
- set val(netif)        Phy/WirelessPhy          ;# network interface type
- set val(mac)          Mac/802_11               ;# MAC type
- set val(nn)           6                        ;# number of mobilenodes
- set val(rp)           AODV                     ;# routing protocol
- set val(x)            800
- set val(y)            800
+set val(chan)           Channel/WirelessChannel    ;#Channel Type
+set val(prop)           Propagation/TwoRayGround   ;# radio-propagation model
+set val(netif)          Phy/WirelessPhy            ;# network interface type WAVELAN DSSS 2.4GHz
+set val(mac)            Mac/802_11                 ;# MAC type
+set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type
+set val(ll)             LL                         ;# link layer type
+set val(ant)            Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen)         50                         ;# max packet in ifq
+set val(nn)             6                          ;# number of mobilenodes
+set val(rp)             AODV                       ;# routing protocol
+set val(x)  500   ;# in metres
+set val(y)  500   ;# in metres
+#Adhoc OnDemand Distance Vector
 
-
-
+#creation of Simulator
 set ns [new Simulator]
-#ns-random 0
 
-set f [open out.tr w]
-$ns trace-all $f
-set namtrace [open out.nam w]
-$ns namtrace-all-wireless $namtrace $val(x) $val(y)
+#creation of Trace and namfile 
+set tracefile [open wireless.tr w]
+$ns trace-all $tracefile
+
+#Creation of Network Animation file
+set namfile [open wireless.nam w]
+$ns namtrace-all-wireless $namfile $val(x) $val(y)
+
+#create topography
 set topo [new Topography]
-$topo load_flatgrid 800 800
+$topo load_flatgrid $val(x) $val(y)
 
+#GOD Creation - General Operations Director
 create-god $val(nn)
 
-set chan_1 [new $val(chan)]
-set chan_2 [new $val(chan)]
-set chan_3 [new $val(chan)]
-set chan_4 [new $val(chan)]
-set chan_5 [new $val(chan)]
-set chan_6 [new $val(chan)]
+set channel1 [new $val(chan)]
+set channel2 [new $val(chan)]
+set channel3 [new $val(chan)]
 
-# CONFIGURE AND CREATE NODES
+#configure the node
+$ns node-config -adhocRouting $val(rp) \
+  -llType $val(ll) \
+  -macType $val(mac) \
+  -ifqType $val(ifq) \
+  -ifqLen $val(ifqlen) \
+  -antType $val(ant) \
+  -propType $val(prop) \
+  -phyType $val(netif) \
+  -topoInstance $topo \
+  -agentTrace ON \
+  -macTrace ON \
+  -routerTrace ON \
+  -movementTrace ON \
+  -channel $channel1 
 
-$ns node-config  -adhocRouting $val(rp) \
-          -llType $val(ll) \
-                 -macType $val(mac) \
-                 -ifqType $val(ifq) \
-                 -ifqLen $val(ifqlen) \
-                 -antType $val(ant) \
-                 -propType $val(prop) \
-                 -phyType $val(netif) \
-                 #-channelType $val(chan) \
-                 -topoInstance $topo \
-                 -agentTrace ON \
-                 -routerTrace ON \
-                 -macTrace ON \
-                 -movementTrace OFF \
-                 -channel $chan_1
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+$n0 random-motion 0
+$n1 random-motion 0
+$n2 random-motion 0
+$n3 random-motion 0
+$n4 random-motion 0
+$n5 random-motion 0
+
+$ns initial_node_pos $n0 20
+$ns initial_node_pos $n1 20
+$ns initial_node_pos $n2 20
+$ns initial_node_pos $n3 20
+$ns initial_node_pos $n4 20
+$ns initial_node_pos $n5 50
+
+#initial coordinates of the nodes 
+$n0 set X_ 10.0
+$n0 set Y_ 20.0
+$n0 set Z_ 0.0
+
+$n1 set X_ 210.0
+$n1 set Y_ 230.0
+$n1 set Z_ 0.0
+
+$n2 set X_ 100.0
+$n2 set Y_ 200.0
+$n2 set Z_ 0.0
+
+$n3 set X_ 150.0
+$n3 set Y_ 230.0
+$n3 set Z_ 0.0
+
+$n4 set X_ 430.0
+$n4 set Y_ 320.0
+$n4 set Z_ 0.0
+
+$n5 set X_ 270.0
+$n5 set Y_ 120.0
+$n5 set Z_ 0.0
+#Dont mention any values above than 500 because in this example, we use X and Y as 500,500
+
+#mobility of the nodes
+#At what Time? Which node? Where to? at What Speed?
+$ns at 1.0 "$n1 setdest 490.0 340.0 25.0"
+$ns at 1.0 "$n4 setdest 300.0 130.0 5.0"
+$ns at 1.0 "$n5 setdest 190.0 440.0 15.0"
+#the nodes can move any number of times at any location during the simulation (runtime)
+$ns at 20.0 "$n5 setdest 100.0 200.0 30.0"
+
+#creation of agents
+set tcp [new Agent/TCP]
+set sink [new Agent/TCPSink]
+$ns attach-agent $n0 $tcp
+$ns attach-agent $n5 $sink
+$ns connect $tcp $sink
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns at 1.0 "$ftp start"
+
+set udp [new Agent/UDP]
+set null [new Agent/Null]
+$ns attach-agent $n2 $udp
+$ns attach-agent $n3 $null
+$ns connect $udp $null
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$ns at 1.0 "$cbr start"
+
+$ns at 30.0 "finish"
 
 proc finish {} {
-    global ns namtrace
-    $ns flush-trace
-        close $namtrace  
-        exec nam -r 5m out.nam &
-    exit 0
+ global ns tracefile namfile
+ $ns flush-trace
+ close $tracefile
+ close $namfile
+ exit 0
 }
 
-# define color index
-$ns color 0 blue
-$ns color 1 red
-$ns color 2 chocolate
-$ns color 3 red
-$ns color 4 brown
-$ns color 5 tan
-$ns color 6 gold
-$ns color 7 black
-                       
-set n(0) [$ns node]
-$ns at 0.0 "$n(0) color blue"
-$n(0) color "0"
-$n(0) shape "circle"
-set n(1) [$ns node]
-$ns at 0.0 "$n(1) color black"
-#$ns at 2.0 "$n(1) color red"
-#$ns at 4.0 "$n(1) color black"
-$n(1) color "blue"
-$n(1) shape "circle"
-set n(2) [$ns node]
-$n(2) color "tan"
-$n(2) shape "circle"
-set n(3) [$ns node]
-$n(3) color "red"
-$n(3) shape "circle"
-set n(4) [$ns node]
-$n(4) color "tan"
-$n(4) shape "circle"
-set n(5) [$ns node]
-$ns at 0.0 "$n(5) color blue"
-$n(5) color "red"
-$n(5) shape "circle"
-
-
-for {set i 0} {$i < $val(nn)} {incr i} {
-    $ns initial_node_pos $n($i) 30+i*100
-}
-#$ns at 2.0 "[$n(1) set ragent_] malicious"
-#$ns at 4.0 "[$n(1) set ragent_] non-malicious"   
-         
-$ns at 0.0 "$n(0) setdest 100.0 100.0 3000.0"
-$ns at 0.0 "$n(1) setdest 200.0 200.0 3000.0"
-$ns at 0.0 "$n(2) setdest 300.0 200.0 3000.0"
-$ns at 0.0 "$n(3) setdest 400.0 300.0 3000.0"
-$ns at 0.0 "$n(4) setdest 500.0 300.0 3000.0"
-$ns at 0.0 "$n(5) setdest 600.0 400.0 3000.0"
-#$ns at 2.9 "$n(1) setdest 700.0 700.0 3000.0"
-
-# CONFIGURE AND SET UP A FLOW
-
-set sink0 [new Agent/LossMonitor]
-set sink1 [new Agent/LossMonitor]
-set sink2 [new Agent/LossMonitor]
-set sink3 [new Agent/LossMonitor]
-set sink4 [new Agent/LossMonitor]
-set sink5 [new Agent/LossMonitor]
-$ns attach-agent $n(0) $sink0
-$ns attach-agent $n(1) $sink1
-$ns attach-agent $n(2) $sink2
-$ns attach-agent $n(3) $sink3
-$ns attach-agent $n(4) $sink4
-$ns attach-agent $n(5) $sink5
-
-#$ns attach-agent $sink2 $sink3
-set tcp0 [new Agent/TCP]
-$ns attach-agent $n(0) $tcp0
-set tcp1 [new Agent/TCP]
-$ns attach-agent $n(1) $tcp1
-set tcp2 [new Agent/TCP]
-$ns attach-agent $n(2) $tcp2
-set tcp3 [new Agent/TCP]
-$ns attach-agent $n(3) $tcp3
-set tcp4 [new Agent/TCP]
-$ns attach-agent $n(4) $tcp4
-set tcp5 [new Agent/TCP]
-$ns attach-agent $n(5) $tcp5
-
-
-proc attach-CBR-traffic { node sink size interval } {
-   #Get an instance of the simulator
-   set ns [Simulator instance]
-   #Create a CBR  agent and attach it to the node
-   set cbr [new Agent/CBR]
-   $ns attach-agent $node $cbr
-   $cbr set packetSize_ $size
-   $cbr set interval_ $interval
-
-   #Attach CBR source to sink;
-   $ns connect $cbr $sink
-   return $cbr
-  }
-
-set cbr0 [attach-CBR-traffic $n(0) $sink5 1000 .030]
-$ns at 0.5 "$cbr0 start"
-set cbr12 [attach-CBR-traffic $n(0) $sink5 1000 0.30]
-$ns at 3.0 "$cbr12 start"
-$ns at 5.5 "finish"
-puts "Start of simulation.."
+puts "Starting Simulation"
 $ns run
